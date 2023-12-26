8bfe68d7ea563c20ece3e3d542f0ddd8e5b8f804
zhao <jazhawang@gmail.com>
2023-12-25 19:55:29 -0500

    wip

--- src/pico/sketch/sketch.ino
+++ src/pico/sketch/sketch.ino
@@ -9,6 +9,8 @@
 */
 
 #include <I2S.h>
+#include <SPI.h>
+#include <SD.h>
 
 // Create the I2S port using a PIO state machine
 I2S i2s(OUTPUT);
@@ -18,12 +20,17 @@ I2S i2s(OUTPUT);
 #define pWS (pBCLK+1)
 #define pDOUT 28
 
-// GPI pin numbers for main buttons
+// GPIO pin numbers for main buttons
 #define pB1 12
 #define pB2 13
 #define pB3 14
 #define pB4 15
 
+// GPIO for sd card in spi mode (pico w tested only, you may need to change this depending on hardware)
+#define pSD_CS 5
+#define pSD_SCK 6
+#define pSD_MOSI 7
+#define pSD_MISO 4
 
 const int frequency = 440; // frequency of square wave in Hz
 const int amplitude = 700; // amplitude of square wave
@@ -35,11 +42,74 @@ int16_t sample = amplitude; // current sample value
 int count = 0;
 int led = 1;
 
+bool setUpSD() {
+
+  Serial.print("Initializing SD card...");
+  SPI.setRX(pSD_MISO);
+  SPI.setTX(pSD_MOSI);
+  SPI.setSCK(pSD_SCK);
+
+  if (!SD.begin(pSD_CS)) {
+    Serial.println("initialization failed!");
+    return 1;
+  }
+
+  if (SD.exists("s01.wav")) {
+    Serial.println("s01.wav exists.");
+  } else {
+    Serial.println("s01.wav doesn't exist.");
+  }
+
+  Serial.println("s01.wav opening.");
+  File wav = SD.open("s01.wav");
+
+  if (!wav) {
+    Serial.println("Cannot open wav file");
+    return 1;
+  }
+
+  if (wav.seek(0)) {
+    Serial.println("Failure seeking");
+  }
+
+  Serial.println("Reading header from file");
+  char hdrBuf[100];
+  wav.readBytes(hdrBuf, 100);
+
+  for (int i=0; i < 50; i++) {
+    Serial.print(hdrBuf[i], HEX);
+    Serial.print(" ");
+  }
+
+  Serial.println("");
+
+  for (int i=50; i < 100; i++) {
+    Serial.print(hdrBuf[i], HEX);
+    Serial.print(" ");
+  }
+
+  Serial.println("");
+
+
+  wav.close();
+
+  Serial.println("done!");
+  return 0;
+}
+
+
+
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(115200);
-  Serial.println("I2S simple tone");
+
+  delay(2000);
+
+  Serial.println("Starting");
+
+  setUpSD();
 
   i2s.setBCLK(pBCLK);
   i2s.setDATA(pDOUT);
---
+++ src/pico/sketch/wav.h
@@ -0,0 +1,24 @@
+
+
+class Wav {
+    long fileSize;
+    int numChannels;
+    long sampleRate;
+    int bitsPerSample;
+
+    long fmtChunkSize;
+    int audioFormat;
+
+    long dataChunkOffset;
+    long dataChunkSize;
+
+  public:
+
+  private:
+
+  int nextChunk(int currentOffset) {
+  }
+
+  int currentChunkSize(int currentOffset) {
+  }
+}
